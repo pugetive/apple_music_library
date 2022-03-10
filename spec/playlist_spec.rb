10230e807549fdc2f9f5fc97cffe208330c3ef67
Todd Gehman <github@pugetive.com>
2022-03-10 14:06:57 -0500

    WIP checkpoint: refactoring globals into respective classes

--- spec/playlist_spec.rb
+++ spec/playlist_spec.rb
@@ -2,25 +2,25 @@ RSpec.describe AppleMusicLibrary::Playlist do
 
   before do
     @library = AppleMusicLibrary.new(VALID_LIBRARY_PATH, VERBOSE)
+    @playlist = @library.playlists.first
   end
 
-  # it "stores and retrieves tracks from playlists" do
-  #   playlist = @library.playlists.first
-  #   expect(playlist.tracks.any?).to be true
-  #   expect(playlist.tracks.first).to be_a(AppleMusicLibrary::Track)
-  # end
+  it "stores and retrieves tracks from playlists" do
+    expect(@playlist.tracks.any?).to be true
+    expect(@playlist.tracks.first).to be_a(AppleMusicLibrary::Track)
+  end
 
-  # it "retrieves a playlist by name" do
-  #   playlist = @library.playlist('XTC Favorites')
-  #   expect(playlist).to be_a(AppleMusicLibrary::Playlist)
-  #   expect(playlist.tracks.any?).to be true
-  # end
+  it "retrieves a playlist by name" do
+    playlist = @library.playlist('XTC Favorites').first
+    expect(playlist).to be_a(AppleMusicLibrary::Playlist)
+    expect(playlist.tracks.any?).to be true
+  end
 
-  # it "excludes folders from list of playlists" do
-  #   @library.playlists.each do |playlist|
-  #     expect(playlist.folder?).to be false
-  #   end
-  # end
+  it "excludes folders from list of playlists" do
+    @library.playlists.each do |playlist|
+      expect(playlist.folder?).to be false
+    end
+  end
 
 
 end
