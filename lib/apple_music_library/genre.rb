0314ef6c39084de32a3f144a9f37bc3fecd82f08
Todd Gehman <github@pugetive.com>
2022-03-13 12:59:22 -0400

    refactoring shared code into TrackCollection - genres done

--- lib/apple_music_library/genre.rb
+++ lib/apple_music_library/genre.rb
@@ -1,29 +1,6 @@
 module AppleMusicLibrary
   class Genre < TrackCollection
 
-    attr_reader :name
-
-    @@genres = {}
-
-    def initialize(name)
-      @name = name
-      super
-    end
-
-    def self.all
-      @@genres.values
-    end
-    
-    def self.find_or_create(name)
-      if @@genres[name]
-        return @@genres[name]
-      end
-      @@genres[name] = self.new(name)
-    end
-
-    def self.find_by_name(name)
-      @@genres[name]
-    end
 
   end
 end
--- lib/apple_music_library/track_collection.rb
+++ lib/apple_music_library/track_collection.rb
@@ -3,9 +3,26 @@ module AppleMusicLibrary
 
     attr_reader :name, :tracks
 
+    @@track_collections = {}
+
     def initialize(name, *args)
       @tracks = []
       @name = name
+    end
+
+    def self.all
+      @@track_collections[token].values
+    end
+
+    def self.find_by_name(lookup_name)
+      @@track_collections[token][lookup_name]
+    end
+
+    def self.find_or_create(lookup_name)
+      unless @@track_collections[token]
+        @@track_collections[token] = {}
+      end
+      @@track_collections[token][lookup_name] = self.new(lookup_name)
     end
 
     def add_track(track)
@@ -21,6 +38,14 @@ module AppleMusicLibrary
 
     def rated_tracks
       @rated_tracks ||= tracks.select{|t| t.rated?}
+    end
+
+    def self.token
+      self.class.name
+    end
+
+    def token
+      self.class.token
     end
 
   end
