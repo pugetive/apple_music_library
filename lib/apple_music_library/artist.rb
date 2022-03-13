5674d716895d4b74738cc4e494bdfbbadb8752f7
Todd Gehman <github@pugetive.com>
2022-03-13 13:47:51 -0400

    more options for sorting artists

--- lib/apple_music_library/artist.rb
+++ lib/apple_music_library/artist.rb
@@ -16,11 +16,15 @@ module AppleMusicLibrary
     end
 
     def self.most_played(limit = 20)
-      if limit.nil?
-        self.all.sort_by(&:play_count).reverse
-      else
-        self.all.sort_by(&:play_count).reverse.take(limit)
-      end
+      self.sorted_set(:play_count, limit)
+    end
+
+    def self.most_tracks(limit = 20)
+      self.sorted_set(:track_count, limit)
+    end
+
+    def self.most_albums(limit = 20)
+      self.sorted_set(:album_count, limit)
     end
 
     def self.find_or_create(name)
@@ -53,13 +57,30 @@ module AppleMusicLibrary
       @albums.sort_by(&:year)
     end
 
+    def album_count
+      @albums.size
+    end
+
     def tracks
       @tracks
+    end
+
+    def track_count
+      @tracks.size
     end
 
     def play_count
       tracks.map(&:play_count).sum
     end
 
+    protected
+
+    def self.sorted_set(sort_by_method, limit)
+      if limit.nil?
+        self.all.sort_by{|a| a.send(sort_by_method)}.reverse
+      else
+        self.all.sort_by{|a| a.send(sort_by_method)}.reverse.take(limit)
+      end
+    end
   end
 end
